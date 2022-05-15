2df11b492ef04c1fb7a817f43d46c5cfbbc6fbe9
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-05-15 14:00:45 -0400

    display list of planets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [planets, setPlanets] = useState([]);
+	const [next, setNext] = useState('https://swapi.dev/api/planets/');
+
+	useEffect(() => {
+		getPlanets(next);
+	}, [next]);
+
+	const getPlanets = async (url) => {
+		const response = await fetch(url);
+		const data = await response.json();
+
+		setPlanets(data.results.concat(planets));
+		setNext(data.next);
+	}
+
+	const planet = planets.map((planet => {
+		return (
+		<li key={planet.name}>
+			{planet.name}
+		</li>
+		)
+	}));
+
+	return (
+		<div className="App">
+			{planet}
+		</div>
+	);
+};
 
 export default App;
